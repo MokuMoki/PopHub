4c575bb236ffbe2de93bf1ae226ab081f2ddfb96
Cheng Wei Lun <14260598+MokuMoki@users.noreply.github.com>
2021-08-14 18:24:28 +0800

    Update popcat_bot.js

--- popcat_bot.js
+++ popcat_bot.js
@@ -1,19 +1,31 @@
+/* 
+1 - Open "popcat.click" website.
+2 - Open Console (Ctrl + Shift + I).
+3 - Copy from line 9 to 31, paste in Console and press Enter to run.
+
+Additional info: https://github.com/MokuMoki/popcat_bot
+*/
+
 let total = 0;
 const count = 800;
 const token = 'mokumoki';
+
+console.clear();
+console.log("%cYour bot have started.", "color: #ff77ff");
+
 setInterval(async () => {
     const res = await fetch(`https://stats.popcat.click/pop?pop_count=${count}&captcha_token=${token}`);
-    const data = await res.json();
     switch (res.status) {
         case 201:
+            const data = await res.json();
             total += count;
-            console.log(`%cSuccessfully sent ${count} pops！You sent ${total} pops in total！（Country：${data.Location.Name}）`, "color: #0f0");
+            console.log(`%cSuccessfully sent ${count} pops！You sent ${total} pops in total！（Country：${data.Location.Name}）`, "color: #6ea561");
             break;
         case 429:
-            console.log("%cRespond too frequent, please wait at least 30 seconds.", "color: #a00");
+            console.log("%cRespond too frequent, please wait at least 30 seconds.", "color: #fbb40c");
             break;
         case 503:
-            console.log("%cYour IP/device is blacklisted by CloudFlare, please wait for 24 hours", "color: #a00");
+            console.log("%cYour IP/device is blacklisted by CloudFlare, please wait for 12 hours.", "color: #de2910");
             break;
     }
 }, 30000);
