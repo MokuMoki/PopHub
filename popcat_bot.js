fb87c207a71c54bda09205cd03682c85868ad00e
wooethan <wooethan@hotmail.com>
2021-08-14 21:51:21 +0800

    Implemented error handling

--- popcat_bot.js
+++ popcat_bot.js
@@ -1,12 +1,12 @@
 /* 
 1 - Open "popcat.click" website.
 2 - Open Console (Ctrl + Shift + I).
-3 - Copy from line 9 to 31, paste in Console and press Enter to run.
+3 - Copy from line 9 to 37, paste in Console and press Enter to run.
 
 Additional info: https://github.com/MokuMoki/popcat_bot
 */
 
-let total = 0;
+let total = 0, error = 0;
 const count = 800;
 const token = 'mokumoki';
 
@@ -19,13 +19,19 @@ setInterval(async () => {
         case 201:
             const data = await res.json();
             total += count;
+            error = 0;
             console.log(`%cSuccessfully sent ${count} pops！You sent ${total} pops in total！（Country：${data.Location.Name}）`, "color: #6ea561");
             break;
         case 429:
             console.log("%cRespond too frequent, please wait at least 30 seconds.", "color: #fbb40c");
             break;
         case 503:
-            console.log("%cYour IP/device is blacklisted by CloudFlare, please wait for 12 hours.", "color: #de2910");
+            error++;
+            if(error < 10) {
+                console.log("%cThere is an issue. Retrying...", "color: #fbb40c");
+            } else {
+                console.log("%cYour IP/device is blacklisted by CloudFlare, please wait for 12 hours.", "color: #de2910");
+            }
             break;
     }
 }, 30000);
