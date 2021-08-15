f6c21b173872b638507e27ebe1be62e58ba79514
wooethan <wooethan@hotmail.com>
2021-08-15 16:07:27 +0800

    Update popcat_bot.js

--- popcat_bot.js
+++ popcat_bot.js
@@ -1,7 +1,7 @@
 /* 
 1 - Open "popcat.click" website.
 2 - Open Console (Ctrl + Shift + I).
-3 - Copy from line 9 to 50, paste in Console and press Enter to run.
+3 - Copy from line 9 to 53, paste in Console and press Enter to run.
 
 Additional info: https://github.com/MokuMoki/popcat_bot
 */
@@ -15,34 +15,37 @@ console.clear();
 console.log("%cYour bot have started.", "color: #ff77ff");
 
 const callAPI = async () => {
-    vue.interval = false;
-    await vue.$recaptchaLoaded();
-    const recaptchaResponseToken = await vue.$recaptcha('pop');
-    token = token ? `&token=${token}` : '';
-    let url = `https://stats.popcat.click/pop?pop_count=${count}&captcha_token=${recaptchaResponseToken}${token}`;
-    const res = await fetch(url, {
-        method: "POST",
-        mode: "cors",
-    });
-    switch (res.status) {
-        case 201:
-            const data = await res.json();
-            token = data.Token ? data.Token : token;
-            total += count;
-            error = 0;
-            console.log(`%cSuccessfully sent ${count} pops！You sent ${total} pops in total！（Country：${data.Location.Name}）`, "color: #6ea561");
-            break;
-        case 429:
-            console.log("%cRespond too frequent, please wait at least 30 seconds.", "color: #fbb40c");
-            break;
-        case 503:
-            error++;
-            if (error < 10) {
-                console.log("%cThere is an issue. Retrying...", "color: #fbb40c");
-            } else {
-                console.log("%cYour IP/device is blacklisted by CloudFlare, please wait for 12 hours.", "color: #de2910");
-            }
-            break;
+    try {
+        await vue.$recaptchaLoaded();
+        const recaptchaResponseToken = await vue.$recaptcha('pop');
+        token = token ? `&token=${token}` : '';
+        let url = `https://stats.popcat.click/pop?pop_count=${count}&captcha_token=${recaptchaResponseToken}${token}`;
+        const res = await fetch(url, {
+            method: "POST",
+            mode: "cors",
+        });
+        switch (res.status) {
+            case 201:
+                const data = await res.json();
+                token = data.Token ? data.Token : token;
+                total += count;
+                error = 0;
+                console.log(`%cSuccessfully sent ${count} pops！You sent ${total} pops in total！（Country：${data.Location.Name}）`, "color: #6ea561");
+                break;
+            case 429:
+                console.log("%cRespond too frequent, please wait at least 30 seconds.", "color: #fbb40c");
+                break;
+            case 503:
+                error++;
+                if (error < 10) {
+                    console.log("%cThere is an issue. Retrying...", "color: #fbb40c");
+                } else {
+                    console.log("%cYour IP/device is blacklisted by CloudFlare, please wait for 12 hours.", "color: #de2910");
+                }
+                break;
+        }
+    } catch (ex) {
+        console.log("%cThere is an issue. Retrying...", "color: #fbb40c");
     }
 }
 
