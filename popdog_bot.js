25c25f4b3861b9adf4eadc8665f8831b6389a346
Kelvin Chin <31331535+kelvinchin12070811@users.noreply.github.com>
2021-08-16 23:14:00 +0800

    Added popdog scripts.

---
+++ popdog_bot.js
@@ -0,0 +1,33 @@
+/************************************************************************************************************
+ * An experimental autoclicker bot for popdog.clicks
+ * Copy and paste the following codes to browser console and press enter.
+ ***********************************************************************************************************/
+const interval = 5_000;
+const clicksToSend = 200;
+
+setInterval(async () => {
+    try {
+        console.log('fetching...')
+        const res = await fetch('/clicked/v2', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({
+                clicks: clicksToSend,
+                uuid: uuid,
+                username: username,
+            }),
+        });
+        const data = await res.json();
+        myClicks = data.clicks;
+        console.log(`Total clicks sended ${myClicks}.`)
+        prevClicks = myClicks;
+        prevUploadedClicks = myClicks;
+    }
+    catch (err)
+    {
+        console.log(err);
+        console.log('Waiting for next run...');
+    }
+}, interval);
