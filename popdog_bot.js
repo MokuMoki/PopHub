ec06320cdd5ad316710abf26e212c38e574bfd24
MokuMoki <14260598+MokuMoki@users.noreply.github.com>
2021-08-17 03:01:41 +0800

    Update popdog_bot.js

--- popdog_bot.js
+++ popdog_bot.js
@@ -3,11 +3,11 @@
  * Copy and paste the following codes to browser console and press enter.
  ***********************************************************************************************************/
 
-const interval = 5_000;     //5 seconds per request
+const interval = 5_000;     //You may try 3800 for higher efficiency
 const clicksToSend = 2000;  //2000 is the maximum amount of clicks can be incremented per request, no point going higher
 
 console.clear();
-console.log(`%cBot will send ${clicksToSend} clicks every 5 seconds.`, "color: #ff77ff");
+console.log(`%cBot will send ${clicksToSend} clicks every few seconds.`, "color: #ff77ff");
 console.log(`%cYour UUID is ${uuid}.`, "color: #ff77ff");
 
 setInterval(async () => {
