f375566d8216d6395d19552de119fad519cf987d
MokuMoki <14260598+MokuMoki@users.noreply.github.com>
2021-08-18 00:27:53 +0800

    Update poppig_bot.js

--- poppig_bot.js
+++ poppig_bot.js
@@ -1,5 +1,6 @@
 /*
   Simple 800 clicks/10 second bot for poppig.click
+  Please avoid clicking on the pig while script is running to avoid clicks rejected by server
 */
 
 setInterval(function(){ clicked = 800; }, 5000);
